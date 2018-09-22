ab1d35ad50407c546eb5c0063d08c8557b4f7188
Aminata <niangaminata17@gmail.com>
2018-09-22 16:31:37 +0000

    version stable du 22 09 2018

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Bnbc\UploadBundle\BnbcUploadBundle(),
+            new FOS\MessageBundle\FOSMessageBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
