acbd5f6823d23728b3f43f304dfe9ddc7e62105c
Sény Mbaye <senyr9@gmail.com>
2018-09-22 20:24:56 +0000

    version stable du 22 09 2018 a 20h

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -12,9 +12,43 @@ class AdminController extends Controller
      */
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository('AppBundle:User')->findAll();
+
         return $this->render('AppBundle:Admin:index.html.twig', array(
-            // ...
+            "users" => $users
         ));
+    }
+
+    /**
+     * @Route("/enabled/{status}/{user}",name="enabled")
+     */
+    public function enabledAction($status, $user) {
+        $em = $this->getDoctrine()->getManager();
+
+        $user_selected = $em->getRepository('AppBundle:User')->find($user);
+        $username = $user_selected->getUsername();
+
+        // Retrieve flashbag from the controller
+        $flashbag = $this->get('session')->getFlashBag();
+
+        if($status == 1) {
+            $user_selected->setEnabled(true);
+            // Add flash message
+            $flashbag->add("success_approve", "L'utilisateur '$username' a été activé !");
+        }
+        if($status == 0) {
+            $user_selected->setEnabled(false);
+            // Add flash message
+            $flashbag->add("failed_approve", "L'utilisateur '$username' a été désactivé !");
+        }
+
+        $em->persist($user_selected);
+        $em->flush();
+
+
+        return $this->redirectToRoute('admin_index') ;
     }
 
 }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -6,6 +6,8 @@ namespace AppBundle\Entity;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use FOS\MessageBundle\Model\ParticipantInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
@@ -20,9 +22,88 @@ class User extends BaseUser implements ParticipantInterface
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Entrez votre nom", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $lastname;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Entrez votre prénom", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $firstname;
+
+
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
     }
 }
